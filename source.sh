47b01da1ea6305b65ddb461ecbc68b201177d00a
azoth07 <azoth07@gmail.com>
2021-10-18 20:04:34 +0800

    Update source.sh

--- source.sh
+++ source.sh
@@ -11,11 +11,7 @@ echo deb "http://mirrors.aliyun.com/ubuntu/ focal-security main restricted unive
 echo deb "http://mirrors.aliyun.com/ubuntu/ focal-updates main restricted universe multiverse" >> /etc/apt/sources.list
 echo deb "http://mirrors.aliyun.com/ubuntu/ focal-proposed main restricted universe multiverse" >> /etc/apt/sources.list
 echo deb "http://mirrors.aliyun.com/ubuntu/ focal-backports main restricted universe multiverse" >> /etc/apt/sources.list
-echo deb-src "http://mirrors.aliyun.com/ubuntu/ focal main restricted universe multiverse" >> /etc/apt/sources.list
-echo deb-src "http://mirrors.aliyun.com/ubuntu/ focal-security main restricted universe multiverse" >> /etc/apt/sources.list
-echo deb-src "http://mirrors.aliyun.com/ubuntu/ focal-updates main restricted universe multiverse" >> /etc/apt/sources.list
-echo deb-src "http://mirrors.aliyun.com/ubuntu/ focal-proposed main restricted universe multiverse" >> /etc/apt/sources.list
-echo deb-src "http://mirrors.aliyun.com/ubuntu/ focal-backports main restricted universe multiverse" >> /etc/apt/sources.list
+
 echo "UPGRADE apt"
 apt-get update
 echo "AUTOREMOVE apt"
